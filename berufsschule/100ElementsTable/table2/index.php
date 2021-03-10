8df0036669ee6ddc1e03ee91797291a28d978f53
Benno Kohrs <bennokohrs@gmail.com>
2021-03-10 15:02:39 +0100

    added table2

--- berufsschule/100ElementsTable/table2/index.php
+++ berufsschule/100ElementsTable/table2/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Table 1</title>
+    <title>Table 2</title>
     <link rel="stylesheet" href="styles.css" />
 </head>
 
@@ -12,13 +12,27 @@
 
     <table border="1" class="TableContainer">
         <?php
+        function isPrime($n)
+        {
+            // Corner case
+            if ($n <= 1)
+                return false;
+
+            // Check from 2 to n-1
+            for ($i = 2; $i < $n; $i++)
+                if ($n % $i == 0)
+                    return false;
+
+            return true;
+        }
+
         for ($i = 1; $i <= 100; $i++) {
             // Table brak if number is modulu 10
             if ($i % 10 == 1) {
                 echo "<tr>";
             }
 
-            echo "<td>" . $i . "</td>";
+            echo "<td " . (isPrime($i) ? "class='yellow'" : '') . ">"  . $i . "</td>";
         }
         ?>
     </table>
