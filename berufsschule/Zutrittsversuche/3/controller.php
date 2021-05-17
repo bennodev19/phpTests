3fd2050f5076ee8616b54275a2e3e821eccab459
Benno Kohrs <bennokohrs@gmail.com>
2021-05-17 11:51:44 +0200

    added basic table

--- berufsschule/Zutrittsversuche/3/controller.php
+++ berufsschule/Zutrittsversuche/3/controller.php
@@ -60,19 +60,36 @@ function closeMySqlConnection($connection)
     mysqli_close($connection);
 }
 
-function getZutrittsversuche()
+function getZutrittsversuche($limit = null)
 {
     // Create SQL Connection
     $sqlConnection = createMySqlConnection('rfidv5');
 
     // Create SQL Query
-    $sqlQuery = "SELECT b.Nachname FROM tblBenutzer as b;";
+    $sqlQuery = "
+    SELECT z.ZutrittsversuchID Id, 
+    z.Zeitstempel as time, 
+    c.ChipsID as chipId, 
+    b.Nachname as chipOwner, 
+    r.Bezeichnung AS door, 
+    z.Ergebnis as result
+    FROM tblZutrittsversuche as z
+    INNER JOIN tblChips as c 
+    ON z.tblChips_ChipsID = c.ChipsID
+    INNER JOIN tblBenutzer as b
+    ON c.tblBenutzer_BenutzerID = b.BenutzerID
+    INNER JOIN tblReader as r
+    ON r.ReaderID = z.tblReader_ReaderID
+    GROUP BY z.ZutrittsversuchID
+    " . (isset($limit) ? (" LIMIT " . $limit . ";") : ";");
 
     // Post Query to SQL Connection
     $result = mysqli_query($sqlConnection, $sqlQuery);
 
     // Close SQL Connection
     closeMySqlConnection($sqlConnection);
+
+    console_log($result);
 
     return $result != false ? $result : null;
 }
--- berufsschule/Zutrittsversuche/3/screens/Zutrittsversuche.php
+++ berufsschule/Zutrittsversuche/3/screens/Zutrittsversuche.php
@@ -7,10 +7,6 @@ require "../controller.php";
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-console_log(getZutrittsversuche());
-?>
-
 <head>
     <meta charset="UTF-8">
     <title>Zutrittsversuche</title>
@@ -26,7 +22,46 @@ console_log(getZutrittsversuche());
 
     <!-- Content -->
     <div class="ContentContainer">
-        <p>Nothing to show?</p>
+        <?php
+        // Get Zutrittsversuche
+        $zutrittsVersuche = getZutrittsversuche(10);
+
+        ?>
+        <table>
+            <tr style="background-color: lightgrey;">
+                <th>ID</th>
+                <th>Uhrzeit</th>
+                <th>Chip</th>
+                <th>Chip Besitzer</th>
+                <th>Tür</th>
+                <th>Ergebnis</th>
+            </tr>
+            <?php
+
+            foreach ($zutrittsVersuche as $zutrittsVersuch) {
+                console_log($zutrittsVersuch);
+
+                $id = $zutrittsVersuch["Id"];
+                $time = $zutrittsVersuch["time"];
+                $chipId = $zutrittsVersuch["chipId"];
+                $chipOwner = $zutrittsVersuch["chipOwner"];
+                $door = $zutrittsVersuch["door"];
+                $result = $zutrittsVersuch["result"];
+                $resultColor = strpos($result, "gestattet") !== false ? "green" : "red";
+            ?>
+                <tr>
+                    <td><?php echo $id ?></td>
+                    <td><?php echo $time ?></td>
+                    <td><?php echo $chipId ?></td>
+                    <td><?php echo $chipOwner ?></td>
+                    <td><?php echo $door ?></td>
+                    <td style="color:<?php echo $resultColor ?>"><?php echo $result ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+        <p>Es ist zu beachten, dass das RFID System sich noch in der Test-Phase befindet!</p>
     </div>
 
     <!-- Footer -->
